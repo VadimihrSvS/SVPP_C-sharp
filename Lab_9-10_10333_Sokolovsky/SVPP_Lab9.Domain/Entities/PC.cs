aaaee978b45d77318646b20ff0856937182ecad7
VadimihrSvS <95396632+VadimihrSvS@users.noreply.github.com>
2023-07-11 11:52:16 +0300

    Add files via upload

---
+++ Lab_9-10_10333_Sokolovsky/SVPP_Lab9.Domain/Entities/PC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVPP_Lab9.Domain.Entities
+{
+    public class PC
+    {
+        public int PCId { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public DateTime BuildDate { get; set; }
+        public string ImageFileName { get; set; }
+        public int StoreId { get; set; }
+    }
+}
---
+++ Lab_9-10_10333_Sokolovsky/SVPP_Lab9.Domain/Entities/Store.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVPP_Lab9.Domain.Entities
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public DateTime OpenedIn { get; set; }
+
+        public string Phone { get; set; }
+        public int PostalCode { get; set; }
+        public ICollection<PC> PCs { get; set; }
+    }
+}
