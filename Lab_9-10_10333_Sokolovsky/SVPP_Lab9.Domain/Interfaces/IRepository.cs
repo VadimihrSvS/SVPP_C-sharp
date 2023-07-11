b2cbca07171b5bd839ea3fa2459a9210d7788fb9
VadimihrSvS <95396632+VadimihrSvS@users.noreply.github.com>
2023-07-11 11:53:24 +0300

    Add files via upload

---
+++ Lab_9-10_10333_Sokolovsky/SVPP_Lab9.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVPP_Lab9.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+        TEntity Get(int id, params string[] includes);
+        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        bool Delete(int id);
+
+    }
+}
---
+++ Lab_9-10_10333_Sokolovsky/SVPP_Lab9.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SVPP_Lab9.Domain.Entities;
+
+namespace SVPP_Lab9.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<PC> PCRepository { get; }
+        IRepository<Store> StoreRepository { get; }
+        void SaveChanges();
+    }
+}
